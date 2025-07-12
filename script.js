c10307340d8037ba9f82a06b94de103d2369c4b2
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-12 12:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function removeColor() {
+  const select = document.getElementById("colorSelect");
+
+  // Get the selected index
+  const selectedIndex = select.selectedIndex;
+
+  // Remove the selected option if any
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+}
